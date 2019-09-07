4aed78548147d0f2a3a848fe5b1345c194f943fc
vanthang10a9 <levanthang510@gmail.com>
2019-09-07 14:07:00 +0700

    test 1 progressbar

---
+++ app/src/main/java/com/example/progressbar/MainActivity.java
@@ -0,0 +1,39 @@
+
+package com.example.progressbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progress_horizontal);
+        new runProgressBar().run();
+    }
+
+    public class runProgressBar extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i <= 100; i+=3) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                progressBar.setProgress(i);
+            }
+        }
+    }
+}
+
