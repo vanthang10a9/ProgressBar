4fff7fc4752919f651e10625e79b03cfc3484435
vanthang10a9 <levanthang510@gmail.com>
2019-09-09 20:27:38 +0700

    + thêm textview hiện % * lỗi chạy hết vòng for mới hiện view, for 100 mà moi 99 đã dừng

--- app/src/main/java/com/example/progressbar/MainActivity.java
+++ app/src/main/java/com/example/progressbar/MainActivity.java
@@ -1,6 +1,7 @@
 
 package com.example.progressbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -8,31 +9,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ProgressBar progressBar;
+    private ProgressBar mProgressBar;
+    private TextView mTextViewTienDo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressBar = (ProgressBar) findViewById(R.id.progress_horizontal);
+        mTextViewTienDo = (TextView) findViewById(R.id.textviewTienDo);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_horizontal);
         new runProgressBar().start();
     }
 
     public class runProgressBar extends Thread {
         @Override
         public void run() {
-            for (int i = 0; i <= 100; i+=3) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+           runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 100; i+=1) {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        mProgressBar.setProgress(i);
+                        mTextViewTienDo.setText(String.valueOf(i) + "%");
+                    }
                 }
-                progressBar.setProgress(i);
-            }
+            });
         }
     }
 }
